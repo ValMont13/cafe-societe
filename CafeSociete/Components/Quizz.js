a017560772670758c1eb9b9089a8004a3ce7ada8
Valentin Montagne <valentin.montagne@epitech.eu>
2017-11-28 19:32:58 +0100

    Adding login, Quizz

--- CafeSociete/Components/Quizz.js
+++ CafeSociete/Components/Quizz.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, ActivityIndicator, ListView } from 'react-native';
+import Auth from './Auth'
 
 export class Quizz extends React.Component {
     static navigationOptions = {
@@ -11,28 +12,62 @@ export class Quizz extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            isLoading: true,
+        };
         this.score = 0;
+        this.getQuizz();
     }
 
-    getMoviesFromApiAsync()
+    async getQuizz()
     {
-        return fetch('https://facebook.github.io/react-native/movies.json')
-            .then((response) => response.json())
-            .then((responseJson) => { return responseJson.movies; })
-            .catch((error) => { console.error(error); });
+        return fetch(Auth.url + "/api/v1/quizz", {
+            headers: {
+              Authorization: Auth.token
+            },
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default'
+        }).then((response) => response.json())
+            .then((responseJson) => {
+            let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+                this.setState ({
+                    isLoading: false,
+                    dataSource: ds.cloneWithRows(responseJson)
+                });
+            }).catch((error) => {
+                console.error("Test : " + error);
+                this.setState ({
+                    isLoading: false,
+                    dataSource: 'titi'
+                });
+            });
     }
 
     render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={{flex: 1, paddingTop: 20}}>
+                    <ActivityIndicator />
+                </View>
+            );
+        }
+
         return (
-            <View>
-                <Text style={styles.Title}>Quizz</Text>
-            </View>
+            <ScrollView>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) => <Text>{rowData.id}</Text>}
+                />
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    Title: {
+    Questions: {
         fontSize: 30,
+        textAlign: 'center',
+        marginTop: '5%',
     }
 });
